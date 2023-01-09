bf8175c7b23022078c038728d79a7d83c39c3ec4
M4RT4Z4K <116658532+M4RT4Z4K@users.noreply.github.com>
2023-01-09 21:41:47 +0100

    Update zad1.hpp

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -1,8 +1,30 @@
 #include "catch.hpp"
+#include <iostream>
 
-// Zad2
-// tutaj definicja klasy Makaron
-// BEZ definicji metody gotujMakaron (tylko z deklaracją)
+class Makaron {
+public:
+    virtual ~Makaron(){};
+    virtual double ileMaki(unsigned P) const { }
+    static Makaron* gotujMakaron(const std::string& S) {
+        if (S.front() == S.back())
+        {
+            return new Tagliatelle(3.14, 0.42, 0.1);
+        }
+        else
+        {
+            return new Penne;
+        }
+    }
+};
 
-// Zad1
-// tutaj definicja klasy Tagliatelle
+class Tagliatelle: public Makaron
+{
+public:
+    Tagliatelle() : L{.5}, W{.5}, R{.5} {};
+    Tagliatelle(double a, double b, double c) : L{a}, W{b}, R{c} {}
+    double ileMaki(unsigned P) const { return (P * L * W * (1. - R) * C);}
+
+private:
+    double       L, W, R;
+    static const double C;
+};
